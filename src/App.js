b9dff8e8ae2e47ac6b9b0831fdb1e2c1e30e67a8
Tyus Durant <todurant@ncdot.gov>
2021-04-07 14:46:20 -0400

    update url

--- src/App.js
+++ src/App.js
@@ -28,12 +28,12 @@ class App extends Component {
     cityThree: [],
     cityWeather: [],
     fetchingMessage: "Data Loading",
-    failMessage: "Data failed to load, try again later."
+    failMessage: "Data failed to load, try again later.",
   };
   getNCFerries = async () => {
     axios
-      .get("https://gisd14.dot.nc.net/GeoRss/FerryGeoJson.ashx")
-      .then(response => {
+      .get("https://gisd14.dot.nc.net/Ncdotferryfeed/ferrygeojson.ashx")
+      .then((response) => {
         let timeStamp = Date.now();
         this.setState(() => ({
           ncferries: response.data.features,
@@ -42,72 +42,72 @@ class App extends Component {
           terminalPins: ports,
           timeStamp,
           filteredFerries: response.data.features,
-          code: response.data.crs.properties.code
+          code: response.data.crs.properties.code,
         }));
 
         // console.log(response.data)
       })
-      .then(resonse => {
+      .then((resonse) => {
         this.setState(() => ({ isLoading: false }));
         // console.log(this.state.code)
       })
 
-      .catch(error => {
+      .catch((error) => {
         console.log(error);
         this.setState(() => ({
           fetchingMessage:
-            "Data failed to load, the service may be temporarily unavailable. Please try again later."
+            "Data failed to load, the service may be temporarily unavailable. Please try again later.",
         }));
       });
   };
   getCityThree = async () => {
     axios
       .get("https://api.weather.gov/gridpoints/ILM/88,67/forecast ")
-      .then(response => {
+      .then((response) => {
         let cityThree = response.data.properties.periods[0];
-        Object.assign(cityThree, { "cityName": "Wilmington" })
-        this.setState({ cityWeather: [...this.state.cityWeather, cityThree] })
+        Object.assign(cityThree, { cityName: "Wilmington" });
+        this.setState({ cityWeather: [...this.state.cityWeather, cityThree] });
       })
 
-      .catch(error => {
+      .catch((error) => {
         console.log(error);
         this.setState(() => ({
           fetchingMessage:
-            "Data failed to load, the service may be temporarily unavailable. Please try again later."
+            "Data failed to load, the service may be temporarily unavailable. Please try again later.",
         }));
       });
   };
   getCityTwo = async () => {
     axios
       .get("https://api.weather.gov/gridpoints/MHX/44,41/forecast ")
-      .then(response => {
+      .then((response) => {
         let cityTwo = response.data.properties.periods[0];
-        Object.assign(cityTwo, { "cityName": "New Bern" })
-        this.setState({ cityWeather: [...this.state.cityWeather, cityTwo] })
+        Object.assign(cityTwo, { cityName: "New Bern" });
+        this.setState({ cityWeather: [...this.state.cityWeather, cityTwo] });
       })
 
-      .catch(error => {
+      .catch((error) => {
         console.log(error);
         this.setState(() => ({
           fetchingMessage:
-            "Data failed to load, the service may be temporarily unavailable. Please try again later."
+            "Data failed to load, the service may be temporarily unavailable. Please try again later.",
         }));
       });
   };
   getCityOne = async () => {
     axios
       .get("https://api.weather.gov/gridpoints/AKQ/95,27/forecast ")
-      .then(response => {
+      .then((response) => {
         let cityOne = response.data.properties.periods[0];
-        Object.assign(cityOne, { "cityName": "Elizabeth City" })
-        this.setState({ cityWeather: [...this.state.cityWeather, cityOne] })
+        Object.assign(cityOne, { cityName: "Elizabeth City" });
+        this.setState({ cityWeather: [...this.state.cityWeather, cityOne] });
       })
 
-      .catch(error => {
+      .catch((error) => {
         console.log(error);
         this.setState(() => ({
           fetchingMessage:
-            "Data failed to load, the service may be temporarily unavailable. Please try again later."
+            "Data failed to load, the service may be temporarily unavailable. Please try again later.",
         }));
       });
   };
@@ -116,7 +116,7 @@ class App extends Component {
     setTimeout(() => {
       this.setState(() => ({
         fetchingMessage:
-          "Data failed to load, the service may be temporarily unavailable. Please try again later."
+          "Data failed to load, the service may be temporarily unavailable. Please try again later.",
       }));
     }, 4000);
   }
@@ -141,19 +141,19 @@ class App extends Component {
         {/* <Header /> */}
         <div id="content-wrapper">
           {this.state.isLoading === true ||
-            this.state.ncferries.length === 0 ? (
-              <Loader
-                isLoading={this.state.isLoading}
-                failMessage={this.state.failMessage}
-                fetchingMessage={this.state.fetchingMessage}
-                arrayCheck={this.state.ncferries}
-              />
-            ) : (
-              <Map
-                //ncferries={filteredFerries}
-                data={this.state}
-              />
-            )}
+          this.state.ncferries.length === 0 ? (
+            <Loader
+              isLoading={this.state.isLoading}
+              failMessage={this.state.failMessage}
+              fetchingMessage={this.state.fetchingMessage}
+              arrayCheck={this.state.ncferries}
+            />
+          ) : (
+            <Map
+              //ncferries={filteredFerries}
+              data={this.state}
+            />
+          )}
 
           {/* <FerryTable ncferries={this.state.ncferries} /> */}
         </div>
